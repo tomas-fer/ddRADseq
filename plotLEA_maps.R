a79fc199959d92f716e32dadde889677ac056cc8
Tomas Fer <tomas.fer@centrum.cz>
2026-01-27 17:57:02 +0100

    R script for map plotting

--- plotLEA_maps.R
+++ plotLEA_maps.R
@@ -9,7 +9,7 @@ library(plotrix)
 library(RColorBrewer)
 args <- commandArgs()
 clusters <- as.numeric(args[6])
-tbl <- read.table(paste0("K",clusters,".txt"), header=T)
+tbl <- read.table(paste0("K", clusters, ".txt"), header=T)
 my_data <- as.data.frame(tbl)
 pie_cols <- names(tbl[,grep("c", colnames(tbl))])
 K <- length(pie_cols)
@@ -19,8 +19,8 @@ max_total <- max(totals)
 scaling_factor <- 0.15
 my_radii <- (totals / max_total) * scaling_factor
 pie_colors <- RColorBrewer::brewer.pal(K, "Set2")
-pdf(paste0("K",clusters,".pdf"))
-#plot(my_data$x, my_data$y,type = "n",xlim = c(min(my_data$x) - 1, max(my_data$x) + 1),ylim = c(48,53),xlab="",ylab="",main=paste0("K ",K))
+pdf(paste0("K", clusters, ".pdf"))
+#plot(my_data$x, my_data$y, type = "n", xlim = c(min(my_data$x) - 1, max(my_data$x) + 1), ylim = c(48,53), xlab="", ylab="", main=paste0("K ", K))
 map(database= "world", ylim = c(min(my_data$y) - 1, max(my_data$y) + 1), xlim = c(min(my_data$x) - 1, max(my_data$x) + 1), col="grey80", fill=F, lwd=2)
 
 for (i in 1:nrow(my_data)) {
@@ -37,6 +37,6 @@ for (i in 1:nrow(my_data)) {
     )
   }
 }
-legend("topright",legend = pie_cols,fill = pie_colors,title = "Cluster",cex = 0.8)
-title(paste0("K",clusters))
+legend("topright", legend = pie_cols, fill = pie_colors, title = "Cluster", cex = 0.8)
+title(paste0("K", clusters))
 dev.off()
